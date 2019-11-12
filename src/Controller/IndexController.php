79763fa335d61920ef0cf4d5622753015b63c9d8
= <bjoern.pfoster@gmail.com>
2019-11-12 21:56:12 +0100

    Implemented monitoring page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -39,6 +39,34 @@ class IndexController
      */
     public function indexAction(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        return $this->encoder->encode($request, $response, 'Index/index.html.twig');
+        $data = [
+            'hosts' => [
+                [
+                    'name' => 'Server 1',
+                    'img' => 'https://cdn.pixabay.com/photo/2013/07/13/10/17/computer-156948__340.png',
+                ],
+                [
+                    'name' => 'Server 3',
+                    'img' => 'https://cdn.pixabay.com/photo/2013/07/13/10/17/computer-156948__340.png',
+                ],
+                [
+                    'name' => 'Server 2',
+                    'img' => 'https://cdn.pixabay.com/photo/2013/07/13/10/17/computer-156948__340.png',
+                ],
+                ['name' => 'Mail', 'img' => 'https://cdn.pixabay.com/photo/2013/07/13/10/17/computer-156948__340.png'],
+                ['name' => 'Mail 2', 'img' => 'https://cdn.pixabay.com/photo/2013/07/13/10/17/computer-156948__340.png'],
+                ['name' => 'Mail 4', 'img' => 'https://cdn.pixabay.com/photo/2013/07/13/10/17/computer-156948__340.png'],
+                [
+                    'name' => 'Uplink',
+                    'img' => 'https://cdn.pixabay.com/photo/2013/07/13/10/17/computer-156948__340.png',
+                ],
+            ],
+        ];
+
+        $count = round(count($data['hosts']) / 2);
+
+        $data['col_count'] = $count;
+
+        return $this->encoder->encode($request, $response, 'Index/index.html.twig', $data);
     }
 }
